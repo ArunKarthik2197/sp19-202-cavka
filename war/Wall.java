4cc5b1039a4dd9dcdf42e3e45de9dc85b1504500
Aruna Pavan Karthik Munipalle <31876070+ArunKarthik2197@users.noreply.github.com>
2019-05-02 14:17:37 -0700

    Update Wall.java

--- war/Wall.java
+++ war/Wall.java
@@ -14,43 +14,61 @@ public class Wall extends Subject
      */
     private int health=200;
     private int damage;
+    private GreenfootImage img;
+    
+    public void addedToWorld(World world)
+    {
+        img=getImage();
+    }
     public void act() 
     {
+        // Add your action code here.
         if(health<=0)
-            die(this);
-
+        die(this);
     }    
-
+    
     public void causeDamage(ISubject s)
     {
-        if(s instanceof Undead)
-        {
-            damage=s.getDamage();
-            health=health-damage;
-        }
-        else if(s instanceof NightKing)
-        {
-            damage=s.getDamage();
-            health=health-damage;
-        }
-        HealthSet(damage);
-    }
+       if(s instanceof Undead)
+       {
+           damage=s.getDamage();
+           health=health-damage;
+           
+           int aq=(int)(health/2);
+           
+           int transPercent=(int)((aq*255)/100);
+           
+           System.err.println("wall calc transparency: "+transPercent);
 
+           System.err.println("wall transparency before : "+img.getTransparency());
+           img.setTransparency(transPercent);
+           System.err.println("wall transparency after: "+img.getTransparency());
+
+       }
+       else if(s instanceof NightKing)
+       {
+           damage=s.getDamage();
+           health=health-damage;
+           
+       }
+       HealthSet(damage);
+    }
+    
     public void HealthSet(int val)
     {
         notifyObserver(this);
     }
-
+    
     public int getHealth()
     {
         return damage;
     }
-
-    public int getDamage()
+    
+       public int getDamage()
     {
         return damage;
     }
-
+    
     public void setDamage(int val)
     {
         this.damage=val;
