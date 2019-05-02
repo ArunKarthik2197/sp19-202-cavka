2e1f2fe61d80584955ec46171e60ca95646d724c
Aruna Pavan Karthik Munipalle <31876070+ArunKarthik2197@users.noreply.github.com>
2019-05-02 14:10:02 -0700

    Update Dragon.java

--- war/Dragon.java
+++ war/Dragon.java
@@ -16,21 +16,20 @@ public class Dragon extends Actor implements IPlayerFactory
     GifImage gifDrg;
     public Dragon()
     {
-        drg=new GreenfootImage("viserion-2.jpg");
+        drg=new GreenfootImage("dragon-flying.gif");
         drg.scale(80,80);
         gifDrg = new GifImage("dragon-flying.gif");
         gifDrg.resizeImages(80, 80);
         setImage(drg);
     }
-
+    
+    public void attack(ISubject s)
+    {
+        
+    }
     public void act() 
     {
        // setImage(gifDrg.getCurrentImage());
         // Add your action code here.
     }    
-    
-    public void attack() 
-    {
-    //add code here
-    }
 }
